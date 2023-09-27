a42f37d917495a0e09d6fd5c8af219079a108418
amit00986 <aryan.amit0824@gmail.com>
2023-09-27 20:08:41 +0530

    delete soft api

--- src/apis/services/v1/DeleteStudentById/index.js
+++ src/apis/services/v1/DeleteStudentById/index.js
@@ -3,7 +3,7 @@ const StudentData = require('@root/src/apis/models/Student');
 // Service function to delete a student by ID
 const deleteStudentById = async (studentId) => {
   try {
-    const deletedStudent = await StudentData.findByIdAndDelete({ _id: studentId }, { isDeleted: true });
+    const deletedStudent = await StudentData.findByIdAndUpdate({ _id: studentId }, { isDeleted: true });
     return {};
   } catch (error) {
     throw new Error('Failed to delete student');
