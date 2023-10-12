477f6af83ab1966c11b87e962802e3637b284c54
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-12 12:54:10 +0530

    Update Student.js

--- src/apis/models/Student.js
+++ src/apis/models/Student.js
@@ -2,33 +2,128 @@ const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema(
   {
-    userId: String,
-    student_id: String,
-    first_name: String,
-    last_name: String,
-    email: String,
-    phone_number: String,
-    secondaryMobileNumber: String,
-    status: {
-      default: null,
+    userId: {
       type: String
     },
-    address: {
-      street: String,
-      city: String,
-      state: String,
-      postal_code: String,
-      country: String,
-      location: {
-        longitude: {
+
+    student_id: {
+      type: String,
+      unique: true
+    },
+
+    personalDetails: { // personal Details ->
+      first_name:
+      {
+        type: String,
+        default: null
+      },
+
+      last_name: {
+        type: String,
+        default: null
+      },
+
+      fathersName: {
+        type: String,
+        default: null
+      },
+
+      dob: {
+        type: String,
+        default: null
+      },
+
+      email: {
+        type: String,
+        default: null
+      },
+
+      phone_number: {
+        type: String,
+        default: null
+      },
+
+      profileImage: {
+        type: String,
+        default: null
+      },
+
+      aboutUs: {
+        type: String,
+        default: null
+      },
+
+      secondaryMobileNumber: {
+        type: String,
+        default: null
+      },
+
+      address: {
+        street: {
           type: String,
+          default: null
         },
-        latitude: {
+
+        city: {
           type: String,
+          default: null
+        },
+
+        state: {
+          type: String,
+          default: null
+        },
+
+        postal_code: {
+          type: String,
+          default: null
+        },
+
+        country: {
+          type: String,
+          default: null
+        },
+
+        location: {
+          longitude: {
+            type: String,
+            default: '0'
+          },
+
+          latitude: {
+            type: String,
+            default:
+              '0'
+          },
+
         },
       },
+      guardians: [
+        {
+          first_name: {
+            type: String,
+            default: null
+          },
+          last_name: {
+            type: String,
+            default: null
+          },
+          relationship: {
+            type: String,
+            default: null
+          },
+          email: {
+            type: String,
+            default: null
+          },
+          phone_number: {
+            type: String,
+            default: null
+          },
+        },
+      ],
     },
-    date_of_birth: Date,
+
     enrolled_batch: [
       {
         batch_id: String,
@@ -36,49 +131,63 @@ const studentSchema = new mongoose.Schema(
         teacher_id: String,
       },
     ],
-    teacher_id:[{
-      teacher_id:String,
-      subject:String,
-      classes:String,
-    }],
-    guardians: [
-      {
-        first_name: String,
-        last_name: String,
-        relationship: String,
-        email: String,
-        phone_number: String,
-      },
-    ],
-       req_status: [
-    {
-      tid: String,
-      status: String,
-      about: String,
+
+    teacher_id: [{
+      teacher_id: String,
       subject: String,
       classes: String,
-      flag:Boolean,
+    }],
+
+    req_status: [
+      {
+        tid: String,
+        status: String, // accept or reject or request
+        about: String,
+        subject: String,
+        classes: String,
+        flag: Boolean,
+      },
+    ],
+
+    ApplicationStatus: {
+      isProfileCompleted: {
+        type: Boolean,
+        default: false
+      },
+      isEducationalDetailCompleted: {
+        type: Boolean,
+        default: false
+      },
     },
-  ],
+
     assignmentId: [String],
+    
     isDeleted: {
       type: Boolean,
       default: false,
     },
+
     createdBy: {
       type: String,
       default: null,
     },
+
     updatedBy: {
       type: String,
       default: null,
     },
+
     isDeleted: {
       type: Boolean,
       default: false
     },
-    additional_info: String,
+
+    additional_info: {
+      type: String,
+      default: null
+    },
   },
+
   { timestamps: { createdAt: true, updatedAt: true }, versionKey: false }
 );
 
