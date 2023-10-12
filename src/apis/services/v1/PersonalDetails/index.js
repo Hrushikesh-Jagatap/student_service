9ada14a6778be7f38b6eeee30d88eec2cdcd3784
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-12 13:04:38 +0530

    Create index.js

---
+++ src/apis/services/v1/PersonalDetails/index.js
@@ -0,0 +1,23 @@
+const studentData = require('@models/Student');
+const _ = require('lodash');
+// Service function to update a PersonalDetaisl by UserID
+const updatePersonalDetailsById = async (userId, updatedPersonalDetails) => {
+  try {
+    const user = await studentData.findOne({ userId: userId });
+    if (!user) {
+      return "User not found In Db"
+    }
+    const mergedPersonalDetails = _.merge({}, user.personalDetails, updatedPersonalDetails);
+    const updatedStudent = await studentData.findOneAndUpdate(
+      { userId: userId },
+      { $set: { personalDetails: mergedPersonalDetails, "ApplicationStatus.isProfileCompleted": true } },
+      { new: true }
+    );
+    return updatedStudent;
+  } catch (error) {
+    throw new Error('Failed to updatePersonalDetails student');
+  }
+};
+module.exports = {
+  updatePersonalDetailsById,
+};
