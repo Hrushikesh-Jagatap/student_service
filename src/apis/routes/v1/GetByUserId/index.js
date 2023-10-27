0203323bf3d7f9cd4f1513f778f808e26d609398
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-27 20:52:27 +0530

    Update index.js

--- src/apis/routes/v1/GetByUserId/index.js
+++ src/apis/routes/v1/GetByUserId/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
+const { HttpResponseHandler } = require('intelli-utility');
 
 const GetByUserIdController = require('@controllers/v1/GetByUseId')
 
 router.get('/user/:Id',async(req, res, next) => {
     try {
-        const result = await GetByUserIdController.getUserById(req, res, next);
+ const data = await GetByUserIdController.getUserById(req, res, next);
+        HttpResponseHandler.success(req, res, data);
     } catch (error) {
         next(error);
     }    
