abdd4d5d2670aa43eeebdb9df8a0d40ee8c7f75d
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-27 21:08:52 +0530

    Update INVALID_MANAGER.js

--- src/common/libs/ErrorHandler/INVALID_MANAGER.js
+++ src/common/libs/ErrorHandler/INVALID_MANAGER.js
@@ -1,15 +1,15 @@
-const {
-    ErrorHandler: { BaseError },
-} = require('intelli-utility');
+// const {
+//     ErrorHandler: { BaseError },
+// } = require('intelli-utility');
 
-class INVALID_MANAGER extends BaseError {
-    constructor() {
-        const code = 'INVALID_MANAGER';
-        const statusCode = '500';
-        const message = `You can't create this employee under this manager.`;
-        const key = 'ums';
-        super({ code, statusCode, message, key });
-    }
-}
+// class INVALID_MANAGER extends BaseError {
+//     constructor() {
+//         const code = 'INVALID_MANAGER';
+//         const statusCode = '500';
+//         const message = `You can't create this employee under this manager.`;
+//         const key = 'ums';
+//         super({ code, statusCode, message, key });
+//     }
+// }
 
-module.exports = INVALID_MANAGER;
+// module.exports = INVALID_MANAGER;
