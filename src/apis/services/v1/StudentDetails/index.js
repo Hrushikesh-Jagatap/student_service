723886c41dc8d8b08367e1ba04f34736383d871b
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-27 20:58:53 +0530

    Update index.js

--- src/apis/services/v1/StudentDetails/index.js
+++ src/apis/services/v1/StudentDetails/index.js
@@ -3,8 +3,14 @@ const StudentData = require('@root/src/apis/models/Student');
 // Service function to get all students
 const getAllStudent = async () => {
   try {
-    const student = await StudentData.find();
-    return student;
+    const data = await StudentData.find();
+    if (data === null) {
+      return {
+        status: 404,
+        message: 'STUDENT_NOT_FOUND IN DB',
+      };
+    }
+    return data;
   } catch (error) {
     throw new Error('Failed to get students');
   }
