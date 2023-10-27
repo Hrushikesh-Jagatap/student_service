3be7790c6491cfdd909663822c994bd9981d79ac
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-27 20:56:09 +0530

    Update index.js

--- src/apis/services/v1/GetByUserId/index.js
+++ src/apis/services/v1/GetByUserId/index.js
@@ -1,15 +1,22 @@
 const StudentData = require('@root/src/apis/models/Student');
-
+const {
+  ErrorHandler: { INTERNAL_SERVER_ERROR },
+} = require('@common/libs');
 // Service function to get a single user by ID
 const getUserById = async (userId) => {
     try {
-      const student = await StudentData.findOne({userId:userId});
-      return student;
-    } catch (error) {
-      throw new Error('Failed to get student By UserId');
+    const data = await StudentData.findOne({ userId: userId });
+    if (data === null) {
+      return {
+        status: 404,
+        message: 'STUDENT_NOT_FOUND',
+      };
     }
-  };
-
+return data;
+  } catch (error) {
+    throw new INTERNAL_SERVER_ERROR;
+  }
+};
 module.exports = {
   getUserById
 }  
