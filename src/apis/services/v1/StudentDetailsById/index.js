6cc4cda38be750fc799c78d8d62b2e4e980887e8
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-27 21:01:54 +0530

    Update index.js

--- src/apis/services/v1/StudentDetailsById/index.js
+++ src/apis/services/v1/StudentDetailsById/index.js
@@ -2,13 +2,20 @@ const StudentData = require('@root/src/apis/models/Student');
 
 // Service function to get a single student by ID
 const getStudentById = async (_id) => {
-    try {
-      const student = await StudentData.findById(_id);
-      return student;
-    } catch (error) {
-      throw new Error('Failed to get student');
+     try {
+    const student = await StudentData.findById(_id);
+    if (student === null) {
+      return {
+        status: 404,
+        message: 'STUDENT_NOT_FOUND',
+      };
     }
-  };
+   return student;
+    
+  } catch (error) {
+    throw new Error('Failed to get student');
+  }
+};
 
 module.exports = {
   getStudentById
