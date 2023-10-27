9b63d7e938b3c1354eae1e724c33d3b0e192c029
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-10-27 21:04:43 +0530

    Update DATA_BASE_ERROR.js

--- src/common/libs/ErrorHandler/DATA_BASE_ERROR.js
+++ src/common/libs/ErrorHandler/DATA_BASE_ERROR.js
@@ -6,8 +6,8 @@ class DATA_BASE_ERROR extends BaseError {
   constructor(msg) {
     const code = 'DATA_BASE_ERROR';
     const statusCode = '502';
-    const message = msg?msg:' Error while querrying the table in ums data_base ';
-    const key = 'ums';
+    const message = msg?msg:' Error while querrying the table in sts data_base ';
+    const key = 'sts';
     super({ code, statusCode, message, key });
   }
 }
